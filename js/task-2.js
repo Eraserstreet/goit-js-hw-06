d0484f37f83d325f019ffac016cc9edccac71fbb
Eraserstreet <148534901+Eraserstreet@users.noreply.github.com>
2024-02-16 19:17:48 +0200

    Home-JS-6.3

--- js/task-2.js
+++ js/task-2.js
@@ -1,19 +1,23 @@
 class Storage {
+  #items;
+
   constructor(items) {
-    this.items = items;
+    this.#items = items;
   }
 
   getItems() {
-    return this.items;
+    return this.#items;
   }
+
   addItem(newItem) {
-    this.items.push(newItem);
+    this.#items.push(newItem);
   }
+
   removeItem(itemToRemove) {
-    const deleteIndex = this.items.indexOf(itemToRemove);
+    const deleteIndex = this.#items.indexOf(itemToRemove);
 
     if (deleteIndex !== -1) {
-      this.items.splice(deleteIndex, 1);
+      this.#items.splice(deleteIndex, 1);
     }
   }
 }
